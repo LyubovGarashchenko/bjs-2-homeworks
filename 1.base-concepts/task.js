f0f407006908cb76ab9aaf023d68c9d080849218
Tinita <tino4ka.87@mail.ru>
2024-03-14 17:42:40 +0300

    Tasks completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
   
-  return arr;
+  if (discriminant === 0) {
+    arr[0] = -b / (2 * a);
+    return arr;
+  } else if (discriminant > 0) {
+    arr[0] = (-b + Math.sqrt(d) ) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d) ) / (2 * a);
+    return arr;
+  } else {
+    return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+let monthPercent = percent / 100 / 12;
+let refundAmount = amount - contribution;
+
+let monthAmount = refundAmount * (monthPercent + (monthPercent / ((Math.pow(1 + monthPercent), countMonths) -1)));
+
+let sum = parseInt(monthAmount * countMonths).toFixed(2);
+
+return sum;
 }
