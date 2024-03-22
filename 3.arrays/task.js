31f405461cc8ee0c48dde19a0136f22f465d2918
Tinita <tino4ka.87@mail.ru>
2024-03-22 19:42:17 +0300

    Tasks 3 completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
+//Задача №1 Сравнить массивы
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
 }
 
+//Задача №2 Получение среднего возраста пользователей одного пола
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const usersFilter = users.filter(user => user.gender === gender);
+
+    if(usersFilter.length === 0){
+        return 0;
+    }
+
+    return usersFilter.reduce((acc, user) => acc + user.age / usersFilter.length, 0);
 }
