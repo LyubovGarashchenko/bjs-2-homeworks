01712ec86b6df6335414273af72a4607257acb30
Tinita <tino4ka.87@mail.ru>
2024-03-26 13:10:03 +0300

    Tasks 4 completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+    if(this.hasOwnProperty('marks')) {
+        return this.marks.push(...marks);
+    } 
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+    if (this.marks && this.marks.length !== 0) {
+        return sum = this.marks.reduce((acc, mark) => acc + mark / this.marks.length, 0);
+    } else {
+        return 0;
+    }
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+
+    this.excluded = reason;
 }
