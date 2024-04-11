4164b5ab4016cc498b48700a21946feb901cf093
Tinita <tino4ka.87@mail.ru>
2024-04-11 19:19:21 +0300

    Tasks 8 completed

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,53 @@
-//Задача № 1
+//Задача 1. Усовершенствовать кеширующий декоратор
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+    function wrapper (...args) {
+        const hash = md5(args); // получаем правильный хеш c помощью функции md5
+        const objectInCache = cache.find(item => item.hash === hash); // ищем элемент, хеш которого равен нашему хешу
+        if(objectInCache) { // если элемент найден
+            console.log("Из кеша: " + objectInCache.value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+            return "Из кеша: " + objectInCache.value;
+        }
+        
+        let result = func(...args); // в кеше результата нет — придётся считать
+        cache.push({
+            hash,
+            value: result
+        }); // добавляем элемент с правильной структурой
+
+        if (cache.length > 5) {
+            cache.shift(); // если слишком много элементов в кеше, надо удалить самый старый (первый) 
+        }
+        
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+    return wrapper;
 }
 
-//Задача № 2
+//Задача № 2. Декоратор debounce с моментальным вызовом и подсчётом количества вызовов
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId;
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+
+    function wrapper(...args) {
+        if(timeoutId) { // если уже есть таймаут
+            clearTimeout(timeoutId);
+        }
+
+        if(!timeoutId) { // первый сигнал
+            func(args);
+            wrapper.count++;
+        }
+
+        timeoutId = setTimeout(() => {
+            wrapper.count++;
+            func(args);
+        }, delay);
+
+        wrapper.allCount++;
+    }
+    return wrapper;
 }
