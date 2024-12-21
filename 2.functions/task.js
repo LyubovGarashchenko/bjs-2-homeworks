5045537418d0482f51debf2148c0878e867d9f0b
LyubovGarashchenko <145012335+LyubovGarashchenko@users.noreply.github.com>
2024-12-21 23:16:42 +0300

    First task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,39 @@
-function getArrayParams(...arr) {
+// Задача 1
+"use strict"
 
-  return { min: min, max: max, avg: avg };
+
+function getArrayParams (...arr) {
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+    }
+    
+    if (arr[i] < min) {
+            min = arr[i];
+    }
+    
+    sum += arr[i];
+    }
+    
+    const avg = Number((sum / arr.length).toFixed(2));
+
+    return {
+    min: min,
+    max: max,
+    avg: avg
+    };
 }
+ 
+ //Tests
+console.log(getArrayParams(-99, 99, 10)) // { min: -99, max: 99, avg: 3.33 }
+console.log(getArrayParams(1, 2, 3, -100, 10)) //{ min: -100, max: 10, avg: -16.80 }
+console.log(getArrayParams(5)) //{ min: 5, max: 5, avg: 5
 
-function summElementsWorker(...arr) {
 
-}
 
-function differenceMaxMinWorker(...arr) {
-
-}
-
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
+  return maxWorkerResult;
 }
