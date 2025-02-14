2333bd5760ef0f2011ec41f12b04139583fd71d2
LyubovGarashchenko <145012335+LyubovGarashchenko@users.noreply.github.com>
2025-02-14 23:22:44 +0300

    Update task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,53 @@
-//Задача № 1
+//Task 1. Усовершенствовать кеширующий декоратор
+
 function cachingDecoratorNew(func) {
-  
+	let cache = [];
+
+	function wrapper(...args) {
+		const hash = md5(args);
+		const objectInCache = cache.find(item => item.hash === hash);
+		if (objectInCache) {
+			console.log("Из кеша: " + objectInCache.value);
+			return "Из кеша: " + objectInCache.value;
+		}
+
+		let result = func(...args);
+		cache.push({
+			hash,
+			value: result
+		});
+		if (cache.length > 5) {
+			cache.shift();
+		}
+		console.log("Вычисляем: " + result);
+		return "Вычисляем: " + result;
+	}
+	return wrapper;
 }
 
-//Задача № 2
+//Task 2. Декоратор debounce с моментальным вызовом и подсчётом количества вызовов
+
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutId;
+	wrapper.count = 0;
+	wrapper.allCount = 0;
+
+	function wrapper(...args) {
+		if (timeoutId) {
+			clearTimeout(timeoutId);
+		}
+
+		if (!timeoutId) {
+			func(args);
+			wrapper.count++;
+		}
+
+		timeoutId = setTimeout(() => {
+			wrapper.count++;
+			func(args);
+		}, delay);
+
+		wrapper.allCount++;
+	}
+	return wrapper;
 }
